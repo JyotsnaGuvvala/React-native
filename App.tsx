ba346e94a40f30e49be5f332941d1de7847cba18
Mula Jyotsna <jyotsna.tarun07@gmail.com>
2023-12-29 13:03:05 +0530

    Manage screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Welcome from "./app/welcome";
-import Dashboard from "./app/Dashboard";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Intro } from "./app/Intro";
-import Mainscreen from "./app/Mainscreen";
+import React from 'react';
+import Welcome from './app/welcome';
+import Dashboard from './app/Dashboard';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Intro} from './app/Intro';
+import Mainscreen from './app/Mainscreen';
+import Manage from './app/Manage';
 
 function App(): React.JSX.Element {
   const Stack = createNativeStackNavigator();
@@ -14,23 +15,31 @@ function App(): React.JSX.Element {
         <Stack.Screen
           name="Intro"
           component={Intro}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          options={{headerShown: false}}></Stack.Screen>
         <Stack.Screen
           name="Dashboard"
           component={Dashboard}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          options={{headerShown: false}}></Stack.Screen>
         <Stack.Screen
           name="Welcome"
           component={Welcome}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          options={{headerShown: false}}></Stack.Screen>
         <Stack.Screen
           name="Mainscreen"
           component={Mainscreen}
-          options={{ headerShown: false }}
-        ></Stack.Screen>
+          options={{headerShown: false}}></Stack.Screen>
+        <Stack.Screen
+          name="Manage"
+          component={Manage}
+          options={{
+            title: 'Manage',
+            headerStyle: {
+              backgroundColor: '#0033cc',
+            },
+            headerTintColor: 'white',
+            
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/Mainscreen.tsx
+++ app/Mainscreen.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import {useNavigation} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
 import {
   Alert,
   Image,
@@ -8,139 +9,83 @@ import {
   Text,
   TouchableOpacity,
   View,
-} from "react-native";
-import Icon from "react-native-ionicons";
-import LinearGradient from "react-native-linear-gradient";
-function Mainscreen(): React.JSX.Element {
-  return (
-    <LinearGradient
-      colors={["#1f1293", "#2520ab", "#282dc3", "#293bdd", "#2749f7"]}
-      style={styles.container}
-    >
-      <View />
-      <View style={styles.header}>
-        <View style={styles.item}>
-          <Image
-            style={styles.icon1 as ImageStyle}
-            source={require("../assets/assets/Addon.png")}
-          />
-          <Text style={styles.text}>Add ons</Text>
-        </View>
-        <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/explore.png")}
-            style={styles.icon as ImageStyle}
-          />
-          <Text style={styles.text}>Explore</Text>
-        </View>
-        <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/standard-bank.png")}
-            style={styles.image as ImageStyle}
-          />
-        </View>
-        <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/help.png")}
-            style={styles.iconhelp as ImageStyle}
-          />
-          <Text style={(styles.text, { marginLeft: 8, color: "#fff" })}>
-            Help
-          </Text>
-        </View>
-        <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/Message.png")}
-            style={styles.icon3 as ImageStyle}
-          />
-          <Text style={styles.text}>Messages</Text>
-        </View>
-      </View>
+} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import Homescreen from './Homescreen';
+import Manage from './Manage';
 
-      <View style={styles.body}>
-        <Text
-          style={{
-            fontSize: 20,
-            color: "#fff",
-            paddingLeft: 10,
-            paddingTop: 10,
-          }}
-        >
-          Accounts
-        </Text>
-        <TouchableOpacity onPress={() => Alert.alert("Accounts Not Linked")}>
-          <Image
-            style={styles.button as ImageStyle}
-            source={require("../assets/assets/linkaccount.png")}
-          />
-          {/*<Text style={{ color: "#fff", fontSize: 15, marginTop: 15 }}>
-            Link your Accounts
-          </Text> */}
-          {/* <View
-            style={{
-              position: "absolute",
-              backgroundColor: "blue",
-              width: "30%",
-              borderTopWidth: 1,
-              borderRightWidth: 1,
-              borderColor: "#fff",
-              zIndex: -1,
-              transform: [{ skewY: "45deg" }],
-              top: -4,
-              right: 0,
-              overflow: "hidden",
-            }}
-          /> */}
-        </TouchableOpacity>
-      </View>
+function Mainscreen(): React.JSX.Element {
+  const navigation = useNavigation();
+  const handleManage = () => {
+    navigation.navigate("Manage")
+  };
+  const handleHome = () => {
+    navigation.navigate("Homescreen")
+  };
+  useEffect(() => {
+    handleHome();
+  
+  }, []);
+
+
+  return (
+    <View style={styles.container}>
+      <Homescreen/>
       <View style={styles.footer}>
         <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/home.png")}
-            style={styles.iconFooter as ImageStyle}
-          />
-          <Text style={styles.textfooter}>Home</Text>
+          <TouchableOpacity
+            onPress={() => {
+              handleHome();
+            }}>
+            <Image
+              source={require('../assets/assets/home.png')}
+              style={[styles.iconFooter, { tintColor: '#0033cc' } as ImageStyle]}
+            />
+            <Text style={[styles.textfooter, { color: '#0033cc' }]}>Home</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.item}>
-          <Image
-            source={require("../assets/assets/Manage.png")}
-            style={styles.iconFooter as ImageStyle}
-          />
-          <Text style={styles.textfooter}>Manage</Text>
+          <TouchableOpacity
+            onPress={() => {
+              handleManage();
+            }}>
+            <Image
+              source={require('../assets/assets/Manage.png')}
+              style={[styles.iconFooter,{marginLeft: 5} as ImageStyle]}
+            />
+            <Text style={styles.textfooter}>Manage</Text>
+          </TouchableOpacity>
         </View>
         <View style={styles.item}>
           <Image
             style={styles.imageFooter as ImageStyle}
-            source={require("../assets/assets/transact.png")}
+            source={require('../assets/assets/transact.png')}
           />
           <Text style={styles.textfooter}>Transact</Text>
         </View>
         <View style={styles.item}>
           <Image
             style={styles.iconFooter2 as ImageStyle}
-            source={require("../assets/assets/cart.png")}
+            source={require('../assets/assets/cart.png')}
           />
           <Text style={styles.textfooter}>Buy</Text>
         </View>
         <View style={styles.item}>
           <Image
             style={styles.iconFooter3 as ImageStyle}
-            source={require("../assets/assets/More.png")}
+            source={require('../assets/assets/More.png')}
           />
-          <Icon name="home-outline" />
-          <Icon ios="ios-add" android="md-albums" size={40} color="orange" />
           <Text style={styles.textfooter}>More</Text>
         </View>
       </View>
-      <StatusBar />
-    </LinearGradient>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   body: {
     flex: 1,
@@ -150,26 +95,30 @@ const styles = StyleSheet.create({
     height: 80,
   },
   header: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     paddingVertical: 10,
   },
   item: {},
   text: {
     fontSize: 15,
-    color: "#fff",
+    color: '#fff',
   },
   textfooter: {
     fontSize: 15,
-    color: "rgb(156 145 145)",
   },
   footer: {
-    flexDirection: "row",
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
     height: 80,
-    justifyContent: "space-around",
-    alignItems: "center",
-    backgroundColor: "#f0f0f0",
+    // Additional footer styles
+    backgroundColor: '#f0f0f0',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
     padding: 10,
   },
 
@@ -195,7 +144,7 @@ const styles = StyleSheet.create({
   iconFooter: {
     width: 30,
     height: 30,
-    marginTop: -20,
+    marginTop: -20,   
   },
   iconFooter2: {
     width: 40,
@@ -215,7 +164,7 @@ const styles = StyleSheet.create({
   imageFooter: {
     width: 60,
     height: 60,
-    marginTop: "-70%",
+    marginTop: '-70%',
     marginBottom: 8,
   },
 });
